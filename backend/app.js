395fd82fb502600830ce2f2535ed3c04cd7472f4
Franz-SuperCode <franzsupercode@gmail.com>
2023-03-09 13:48:53 +0100

    Bilder Funktion base64 fertig

--- backend/app.js
+++ backend/app.js
@@ -10,15 +10,20 @@ import { ObjectId } from 'mongodb';
 const app = express();
 //Den Port holen wir aus der .env-Datei
 const PORT = process.env.PORT;
+//Man braucht multer damit die FormData Daten aus dem Frontend ohne Probleme verwendet werden können
+const upload = multer({ dest: "./public" });
+
+
 //Der Browser blockt alle Adressen die auf den Server zugreifen wollen außer sich selbst. Mit Cors kann man das beheben
 app.use(cors());
 //Fügt die Middleware hinzu, die verwendet wird, um den Request-Body in ein JSON-Objekt zu parsen.
-app.use(express.json())
-//Man braucht multer damit die FormData Daten aus dem Frontend ohne Probleme verwendet werden können
-const upload = multer();
+app.use(express.json());
+//Hat etwas mit Multer und dem Bild zutun
+app.use('/public', express.static('./public'));
 
 
-// -------- Alle Objekte bekommen -------------
+
+//! -------- Alle Objekte bekommen -------------
 app.get('/api/moebel', (req, res) => {
     //Ruft die Funktion "getDb" auf, um eine Verbindung zur MongoDB-Datenbank herzustellen
     getDb()
@@ -30,7 +35,7 @@ app.get('/api/moebel', (req, res) => {
         .then(array => res.status(200).json(array))
 })
 
-//------------Ein Objekt bekommen ---------------
+//!------------Ein Objekt bekommen ---------------
 app.get('/api/moebel/:id', (req, res) => {
     //Ruft die Funktion "getDb" auf, um eine Verbindung zur MongoDB-Datenbank herzustellen
     getDb()
@@ -40,8 +45,9 @@ app.get('/api/moebel/:id', (req, res) => {
         .then(item => item ? res.status(200).json(item) : res.status(404).json({ message: 'Objekt nicht gefunden' }))
 });
 
+//!----Neuen Eintrag hinzufügen (Mit Bild) ------------
+app.post('/api/moebel', upload.single("image"), (req, res) => {
 
-app.post('/api/moebel', upload.none(), (req, res) => {
     const moebel = req.body;
     console.log(moebel);
     getDb()
@@ -50,8 +56,7 @@ app.post('/api/moebel', upload.none(), (req, res) => {
         .then(ark => res.status(200).json(ark))
 })
 
-
-
+//!------Eintrag verändern-------------------
 app.put('/api/moebel/:id', async (req, res) => {
     const params = req.params.id;
     const item = req.body
@@ -69,7 +74,7 @@ app.put('/api/moebel/:id', async (req, res) => {
     res.json(result)
 })
 
-
+//!------Eintrag löschen-------------------
 app.delete('/api/moebel/:id', (req, res) => {
     //Hole die die Info id aus der URL
     const params = req.params.id;
--- frontend/src/components/formular/Formular.jsx
+++ frontend/src/components/formular/Formular.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 
 function Formular(props) {
+//base64 ist das Format in dem Das Bild als String umgewandelt und an MongoDB geschickt wird
+    const [base64, setBase64] = useState('');
+
 
     const sendData = (e) => {
         e.preventDefault()
@@ -7,6 +11,8 @@ function Formular(props) {
         //Mit e.target bekommt man das <form>
         //Und dann packt man es in den body vom Request
         const form = new FormData(e.target);
+  // Add the base64-encoded image to the FormData object
+  form.append("image", base64);
         //Hier wird nun gefetched mit einem POST req an den Server
         fetch('http://localhost:9999/api/moebel', {
             method: 'POST',
@@ -16,15 +22,32 @@ function Formular(props) {
                 if (response.ok) {
                     props.setRefresh(prev => !prev); // hier wird refresh ein neuer wert zugewiesen. Dadurch wird die Seite wo das Formular ist, neu gerendert
                 }
-                response.json();
+              return response.json();
             })
             .then((data) => {
                 console.log(data);
             })
     }
+
+    const handleReaderLoaded = (event) => {
+        setBase64(event.target.result);
+    }
+
+
+    //! Das benutzen wir wenn wir Bilder als base64 string abspeichern wollen
+    const picConverter = (event) => {
+         // Extrahiere das erste ausgewählte File aus dem "event.target.files" Array und speichere es in der Variablen "file"
+        const file = event.target.files[0];
+        const fr = new FileReader();
+        fr.onload = handleReaderLoaded;
+        fr.readAsDataURL(file);
+      }
+      
+    
+
     return (
         //Bilder und Texte, also Daten, müssen noch an den Server gesendet werden
-        <form onSubmit={sendData}>
+        <form encType="multipart/form-data"  onSubmit={sendData}>
             <p>Neue Einträge in die Datenbank</p>
             <input type="text" name="title" placeholder="Title" />
             {/* multer wird nach "picture" Bildern suchen */}
@@ -33,6 +56,8 @@ function Formular(props) {
             <input type="number" name="price" placeholder="Price" />
             <input type="description" name="description" placeholder="description" />
             <input type="submit" value="Publish" />
+            <input type="file" name="image" onChange={picConverter}/>
+            <img src={base64}/>
         </form>
     )
 }
